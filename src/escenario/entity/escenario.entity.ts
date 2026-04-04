5069b75aa2a3c55283f2ece528dd355b460cdbf4
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2026-04-04 10:06:00 -0500

    Se agregaron nuevos campos de 'escenario'

--- src/escenario/entity/escenario.entity.ts
+++ src/escenario/entity/escenario.entity.ts
@@ -1,8 +1,34 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+// El lugar físico donde se reserva.
+
+export enum estado{
+    ACTIVO = 'activo',
+    INACTIVO = 'inactivo',
+
+}
 
 @Entity('escenario')
 export class Escenario {
     @PrimaryGeneratedColumn('increment')
     id: number
-    
+
+    @Column()
+    nombre: string
+
+    @Column()
+    tipo_deporte_id: number // Llave foránea
+
+    @Column()
+    ubicacion_id: number // Llave foránea
+
+    @Column()
+    capacidad_maxima: number
+
+    @Column()
+    descripcion: string
+
+    @Column({type: 'enum', enum: estado, default: estado.INACTIVO})
+    estado: estado
+
 }
