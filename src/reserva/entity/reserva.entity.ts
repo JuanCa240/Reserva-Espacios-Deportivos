1ed0344fd153e1e922037ebd56370074b8fca5db
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2026-04-04 10:32:32 -0500

    Se agregaron los campos de la entidad 'reserva'

--- src/reserva/entity/reserva.entity.ts
+++ src/reserva/entity/reserva.entity.ts
@@ -1,8 +1,39 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+
+export enum estado{
+    PENDIENTE = 'pendiente',
+    CONFIRMADO = 'confirmado',
+    CANCELADA = 'cancelada',
+}
 
 @Entity('reserva')
 export class Reserva {
     @PrimaryGeneratedColumn('increment')
     id: number
+
+    @Column()
+    usuario_id: number // Llave foránea
+
+    @Column()
+    escenario_id: number // Llave foránea
+
+    @Column({type: 'date'})
+    fecha: Date         // TypeORM usa Date para mapear el tipo DATE de la base de datos.
+
+    @Column()
+    hora_inicio: string
+
+    @Column()
+    hora_fin: string
+
+    @Column()
+    cantidad_personas: number
+
+    @Column()
+    precio_total: number
+
+    @Column({type: 'date'})
+    fecha_creacion: Date
     
 }
