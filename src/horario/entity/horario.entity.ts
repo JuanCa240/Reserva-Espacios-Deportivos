d18c4e332d1dfa59b6d7bd0bf68801394e310086
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2026-04-04 10:31:41 -0500

    Se agregaron los campos en la entidad 'horario'

--- src/horario/entity/horario.entity.ts
+++ src/horario/entity/horario.entity.ts
@@ -1,11 +1,22 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-//ubicación del escenario”
+// Aquí definimos cuando se puede usar un escenario (horario).
 
 @Entity('horario')
 export class Horario {
     @PrimaryGeneratedColumn('increment')
     id: number
-   
-    
+
+    @Column()
+    escenario_id: number // Llave foránea
+
+    @Column()
+    diaSemana: string
+
+    @Column()
+    hora_inicio: string
+
+    @Column()
+    hora_fin: string
+ 
 }
