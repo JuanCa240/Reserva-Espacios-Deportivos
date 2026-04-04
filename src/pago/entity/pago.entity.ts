fd99e3ace580e181d01f5ef9a6e8749cf1e5cd00
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2026-04-04 10:47:24 -0500

    Se agregaron los campos de la entidad 'Pago'

--- src/pago/entity/pago.entity.ts
+++ src/pago/entity/pago.entity.ts
@@ -1,8 +1,41 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// Realizar los pagos seguros
+
+export enum metodoPago{
+    NEQUI = 'nequi',
+    TARJETA = 'tarjeta',
+    EFECTIVO = 'efectivo',
+    DAVIPLATA = 'daviplata',
+}
+
+export enum estado{
+    PENDIENTE = 'pendiente',
+    APROBADO = 'aprobado',
+    RECHAZADO = 'rechazado'
+}
 
 @Entity('pago')
 export class Pago {
     @PrimaryGeneratedColumn('increment')
     id: number
+
+    @Column()
+    reserva_id: number
+
+    @Column()
+    monto: number
+
+    @Column({type: 'enum', enum: metodoPago})
+    metodo_pago: metodoPago
+
+    @Column({type: 'enum', enum: estado})
+    estado: estado
+
+    @Column()
+    referencia_pago: string
+
+    @Column({type: 'date'})
+    fecha_pago: Date
     
 }
