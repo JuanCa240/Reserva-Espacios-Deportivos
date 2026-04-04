8b719dcd47512187bf3a66516732245c4d726ac3
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2026-04-04 10:15:15 -0500

    Se añadieron los campos faltantes de 'tipo-deporte'

--- src/tipo-deporte/entity/tipo-deporte.entity.ts
+++ src/tipo-deporte/entity/tipo-deporte.entity.ts
@@ -1,8 +1,22 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// Para controlar “horarios del deporte”.
+
+export enum tiposDeporte{
+    FUTBOL = 'futbol',
+    TENIS = 'tenis',
+    BASQUET = 'basquet',
+    VOLEY = 'voley'
+}
 
 @Entity('tipo-deporte')
 export class TipoDeporte {
     @PrimaryGeneratedColumn('increment')
     id: number
     
+    @Column({type: 'enum', enum: tiposDeporte})
+    tipoDeporte: tiposDeporte
+
+    @Column()
+    descripcion: string
 }
